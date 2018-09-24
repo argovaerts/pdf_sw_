0a0252f6ff4e424dc30a28bc64ae6d55f2931e07
Arne Govaerts <argovaerts@users.noreply.github.com>
2018-09-24 11:02:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -101,8 +101,10 @@ var openPage = function(pdfFile, pageNumber) {
       viewport = page.getViewport(scale);
     }
 
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
+    //canvas.height = viewport.height;
+    //canvas.width = viewport.width;
+    canvas.height = viewport.height * 0,95;
+    canvas.width = viewport.width * 0,95;
 
     var renderContext = {
       canvasContext: context,
